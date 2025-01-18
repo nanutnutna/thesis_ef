68342d1b83f339179d0b2add60aa514679bfea50
nanutnutna <jtnattapot@gmail.com>
2025-01-18 11:43:05 +0700

    modify compare.py and collect_data.py

--- extract_data/collect_data.py
+++ extract_data/collect_data.py
@@ -3,10 +3,11 @@ from bs4 import BeautifulSoup
 import urllib3
 import pandas as pd
 from datetime import datetime
+from io import StringIO
 
 URL_CFP = "https://thaicarbonlabel.tgo.or.th/index.php?lang=TH&mod=Y0hKdlpIVmpkSE5mWlcxcGMzTnBiMjQ9"
 URL_CFO = "https://thaicarbonlabel.tgo.or.th/index.php?lang=TH&mod=YjNKbllXNXBlbUYwYVc5dVgyVnRhWE56YVc5dQ"
-OUTPUT_PATH = "source"
+OUTPUT_PATH = r"C:\Users\Nattapot\Desktop\thesis_ef\extract_data\source"
 
 current_date = datetime.now().strftime("%Y%m%d")
 
@@ -22,13 +23,73 @@ def extract_cfp(url=URL_CFP):
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, 'lxml')
         table = soup.find("table",{"class":"table table-striped table-bordered table-hover table-full-width table-customize"})
-        df = pd.read_html(str(table))[0]
+        df = pd.read_html(StringIO(str(table)))[0]
+        
+        #re-order
+        df = df[['ลำดับ','กลุ่ม','ชื่อ','รายละเอียด','หน่วย','ค่าแฟคเตอร์ (kgCO2e)','ข้อมูลอ้างอิง','วันที่อัพเดท']]
+        df['ประเภทแฟคเตอร์'] = "CFP"
     else:
         print(f"Failed to fetch page, status code: {response.status_code}")
 
-    output = f"{OUTPUT_PATH}/emission_factor_{current_date}.csv"
+    output = f"{OUTPUT_PATH}/emission_factor_cfp_{current_date}.csv"
     df.to_csv(output,index=False,encoding='utf-8-sig')
-    return 
+    return df
 
 def extract_cfo(url=URL_CFO):
-    pass
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+    url = "https://thaicarbonlabel.tgo.or.th/index.php?lang=TH&mod=YjNKbllXNXBlbUYwYVc5dVgyVnRhWE56YVc5dQ"
+
+
+    response = requests.get(url, verify=False)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'lxml')
+        table = soup.find("table")
+        df = pd.read_html(StringIO(str(table)),header=[1,2])[0]
+        df.columns = [" ".join(col).strip() if "unit" in col[1] else col[0] for col in df.columns]
+
+        ## modify table 
+        df['รายละเอียด'] = df['ลำดับ']
+        df['รายละเอียด'] = df['รายละเอียด'].fillna(df['ชื่อ'])
+        df['กลุ่ม'] = df['รายละเอียด']
+        df.loc[1:23, 'รายละเอียด'] = 'Stationary Combustion'
+        df.loc[24:31, 'รายละเอียด'] = 'Mobile Combustion (On road)'
+        df.loc[31:37, 'รายละเอียด'] = 'Mobile Combustion (Off road), Diesel'
+        df.loc[38:42, 'รายละเอียด'] = 'Mobile Combustion (On road), Motor Gasoline 4 stroke'
+        df.loc[43:47, 'รายละเอียด'] = 'Mobile Combustion (On road), Motor Gasoline 2 stroke'
+        df.loc[48, 'รายละเอียด'] = 'Electricity, grid mix (ไฟฟ้า)'
+        df.loc[50:, 'รายละเอียด'] = 'Refrigerants (สารทำความเย็น)'
+        df['ลำดับ'] = pd.to_numeric(df['ลำดับ'], errors='coerce')
+        df = df.rename(columns={'Total [kg CO2eq/unit]':'ค่าแฟคเตอร์ (kgCO2e)'})
+        df = df.dropna(subset=['ลำดับ'])
+        df = df.drop(columns=['CO2 [kg CO2/unit]','Fossil CH4 [kg CH4/unit]','CH4 [kg CH4/unit]','N2O [kg N2O/unit]'])
+        df['วันที่อัพเดท'] = 'Update_Apr2022'
+        #re-order column name 
+        df = df[['ลำดับ','ชื่อ','รายละเอียด','หน่วย','ค่าแฟคเตอร์ (kgCO2e)','ข้อมูลอ้างอิง','วันที่อัพเดท']]
+        df['ประเภทแฟคเตอร์'] = "CFO"
+    else:
+        print(f"Failed to fetch page, status code: {response.status_code}")
+
+    #save table
+    output = f"{OUTPUT_PATH}/emission_factor_cfo_{current_date}.csv"
+    df.to_csv(output,index=False,encoding='utf-8-sig')
+    return df
+
+def mergetable(t1,t2):
+    table = pd.concat([t1,t2],ignore_index=True,)
+    output = f"{OUTPUT_PATH}/emission_factor_{current_date}.csv"
+
+    #save table
+    table.to_csv(output,index=False,encoding='utf-8-sig')
+    return
+
+
+if __name__ == "__main__":
+    print("Extract data from TGO")
+    table_cfp = extract_cfp(url=URL_CFP)
+    print("Extract table CFP")
+    table_cfo = extract_cfo(url=URL_CFO)
+    print("Extact table CF")
+
+    print("concat talbe")
+    mergetable(table_cfp,table_cfo)
+    print("Extract data suscessfully")
--- extract_data/compare.py
+++ extract_data/compare.py
@@ -1,24 +1,35 @@
-import numpy as np
 import pandas as pd
-import time
+from datetime import datetime,timedelta
 
-INPUT_PATH = "source/"
-FILE_NAME = "emission_factor"
+INPUT_PATH = r"C:\Users\Nattapot\Desktop\thesis_ef\extract_data\source"
+FILE_NAME_CFO = "emission_factor_cfo"
+FILE_NAME_CFP = "emission_factor_cfp"
+CONCAT_CFP_CFO = "emission_factor_"
+
+current_date = datetime.now()
+last_date = datetime.now() - timedelta(days=1)
+current_date_str = current_date.strftime("%Y%m%d")
+last_date_str = last_date.strftime("%Y%m%d")
+
+# print(current_date_str,last_date_str)
 
 
+def read_table_csv(filename,date):
+    name = f'{INPUT_PATH}\{filename}_{date}.csv'
+    return pd.read_csv(name)
 
-def read_table():
-    t1 = pd.read_csv("source/emission_factor_1.csv")
-    t2 = pd.read_csv("source/emission_factor_1_test.csv")
-    return t1,t2
-
-
-def compare_table():
-    t1,t2 = read_table()
+def compare_table(table1,table2):
+    t1 = read_table_csv(table1)
+    t2 = read_table_csv(table2)
     if t1.equals(t2):
         return True
-    return t1.compare(t2)
+    return 
 
 
 if __name__ == "__main__":
-    print(compare_table())
+    table1 = read_table_csv(filename=CONCAT_CFP_CFO,date=last_date_str)
+    table2 = read_table_csv(filename=CONCAT_CFP_CFO,date=current_date_str)
+    if compare_table(table1,table2):
+        print("nothing diff")
+    else:
+        table1.compare(table2)
